5e7e797b445ba4968c0a235cd5e978fc7197faa3
Goemans.Stephan <quizdroid@gmail.com>
2014-12-17 17:52:15 +0100

    More Gulp tasks

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,15 @@
 // Include gulp
 var gulp = require("gulp");
 // Include gulp plugins
+var gutil = require("gulp-util");
+var clean = require("gulp-clean");
 var concat = require("gulp-concat");
 var uglify = require("gulp-uglify");
 var sass = require('gulp-sass');
 var usemin = require('gulp-usemin');
 var minifyCss = require("gulp-minify-css");
 var minifyHtml = require("gulp-minify-html");
+var jasminePhantomJs = require('gulp-jasmine2-phantomjs');
 
 gulp.task("sass", function() {
 	return gulp.src("src/scss/*.scss")
@@ -38,10 +41,19 @@ gulp.copy = function(src, dest){
 		.pipe(gulp.dest(dest));
 };
 */
+gulp.task('clean', function() {
+	if(!gutil.env["no-clean"]) {
+		gulp.src(['dist/'], {read: false}).pipe(clean());
+	}
+});
 
 gulp.task("copyImg", function() {
 	return gulp.src("src/img/**/*", {base:"src"})
-		.pipe(gulp.dest("dist"));
+		.pipe(gulp.dest("dist/"));
 });
 
-gulp.task("default", ["copyImg", "usemin"]);
+gulp.task("test", function() {
+	return gulp.src("test/**/*.html")
+		.pipe(jasminePhantomJs());
+});
+gulp.task("default", ["clean", "copyImg", "usemin"]);
